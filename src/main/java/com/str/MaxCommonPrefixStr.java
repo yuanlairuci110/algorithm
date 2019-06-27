1d26291c0ee91bf93bb6788db1839d6d56f85986
lichaoi <lichaoi@yonyou.com>
2019-06-27 18:54:02 +0800

    最长公共前缀

---
+++ src/main/java/com/str/MaxCommonPrefixStr.java
@@ -0,0 +1,26 @@
+package com.str;
+
+import java.util.Scanner;
+
+/**
+ * @author lichaoi <br/>
+ * @date 2019年06月13日  20:01
+ * <p>
+ * 最长公共前缀
+ * <p>
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ */
+public class MaxCommonPrefixStr {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
