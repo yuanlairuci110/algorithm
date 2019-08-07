840c868654585651867fc75e19370da9219cae5d
lichaoi <lichaoi@yonyou.com>
2019-08-07 09:46:19 +0800

    Demo3

---
+++ src/main/java/com/demo/Demo3.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+/**
+ * @author lichaoi <br/>
+ * @date 2019年08月07日  9:45
+ */
+public class Demo3 {
+    public static void main(String[] args) {
+        System.out.println("Demo3");
+    }
+}
